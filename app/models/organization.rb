9a08fe7be36e861d839e3d51585953eac8f8b780
Raghu Betina <raghu@firstdraft.com>
2017-09-26 04:09:15 +0000

    Direct association between organizations and users

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,9 @@
 class Organization < ApplicationRecord
   # Direct associations
 
+  has_many   :users,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  belongs_to :organization,
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
