13d62cf5d7fd0c5d1fb2d538a554f2884f8c19fd
Raghu Betina <raghu@firstdraft.com>
2017-09-26 04:09:43 +0000

    Whitelist columns in Check for ActiveAdmin

--- app/admin/check.rb
+++ app/admin/check.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Check do
 
+ permit_params :check_image, :account_type, :check_number, :grant_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
